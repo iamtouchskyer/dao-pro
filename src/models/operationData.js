2697eec2f9b18b81a22af95d5cdf2a8be01b2539
Yucong Jin <yucongj@microsoft.com>
2018-01-23 03:56:07 -0800

    remove console.log

--- src/models/operationData.js
+++ src/models/operationData.js
@@ -189,8 +189,6 @@ export default {
       };
     },
     updateProvinceFilter(state, { payload }) {
-      window.console.log(state.rawData);
-
       return {
         ...state,
         provinceFilter: {
