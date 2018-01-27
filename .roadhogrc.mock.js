44ac1280a436cae4dc943bb8b369a798c30427d2
touchskyer <touchskyer@touchskyerMacbookPro.local>
2018-01-27 15:23:34 +0800

    Updated the endpoint

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -130,15 +130,13 @@ const proxy = {
       "path": "/base/category/list"
     });
   },
-  'GET /api/cibn/personalrecommendation': CIBN.getFakePersonalRecommendation,
-  'GET /api/cibn/personal/tags': CIBN.getFakePersonalTags,
-  'GET /api/cibn/personal/tag': CIBN.getFakePersonalTag,
-  // 'GET /api/cibn/operationdata': CIBN.getFakeOperationData,
   'GET /api/cibn/operationdata': 'http://localhost:3030/',
   'GET /api/cibn/users': 'http://localhost:3030/',
+  'GET /api/cibn/hot': 'http://localhost:3030/',
 
-  'GET /api/cibn/hot/filter': CIBNHot.getFilterForVideoKind,
-  'GET /api/cibn/hot/playcount': CIBNHot.getPlayCount
+  // 'GET /api/cibn/operationdata': CIBN.getFakeOperationData,
+  // 'GET /api/cibn/hot/filter': CIBNHot.getFilterForVideoKind,
+  // 'GET /api/cibn/hot/playcount': CIBNHot.getPlayCount
 };
 
 export default noProxy ? {} : delay(proxy, 1000);
